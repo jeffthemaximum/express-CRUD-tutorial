93c628bdf548994e4482428a63156734fdeb8dc5
jeffthemaximum <frey.maxim@gmail.com>
2017-08-31 16:22:31 -0400

    C with mongo

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require('express');
+const bodyParser= require('body-parser');
+const MongoClient = require('mongodb').MongoClient
 const app = express();
 
-app.listen(3000, () => {
-  console.log('listening on 3000')
+const Secrets = require("./secrets.js")
+
+// middlewares
+app.use(bodyParser.urlencoded({extended: true}))
+
+// Mongo
+var db
+
+MongoClient.connect(Secrets.dbUrl, (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  // init server
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
 })
 
 app.get('/', (req, res) => {
-  res.send("hello world!")
+  res.sendFile(__dirname + '/index.html')
+})
+
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
